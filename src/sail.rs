918db20debb354fe6f4ee2c0757f63662db17e7e
Matthew Rothlisberger <mattjrothlis@gmail.com>
2020-11-22 19:14:21 -0500

    improves on basic Sail data structures

--- src/sail.rs
+++ src/sail.rs
@@ -2,23 +2,65 @@
 //!
 //! A custom Lisp dialect for writing STARK
 
-struct List {
+use std::fmt;
+
+struct _Cons {
     car: Value,
-    cdr: Option<Box<List>>,
+    cdr: Value,
+}
+
+// TODO: Figure out how to manage / dispose of memory
+pub struct List {
+    car: Value,
+    cdr: *mut List,
+}
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let car = self.car.to_string();
+        let cdr = unsafe {
+            match self.cdr.as_ref() {
+                Some(x) => String::from(" ") + &x.to_string(),
+                None => String::new(),
+            }
+        };
+        write!(f, "{}{}", car, cdr)
+    }
 }
 
 /// Container type for many possible value types
 /// TODO: add support for arbitrary precision numbers (`rug` crate)
-enum Value {
+pub enum Value {
+    // None == nil == empty list ()
+    List(*mut List),
+
     True,
     False,
 
     Symbol(String),
-    List(Box<List>),
 
     String(String),
     Integer(i32),
     Float(f32),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::List(p) => unsafe {
+                match p.as_ref() {
+                    Some(x) => write!(f, "({})", x),
+                    None => write!(f, "()"),
+                }
+            },
+            Value::True => write!(f, "#T"),
+            Value::False => write!(f, "#F"),
+            Value::Symbol(x) => write!(f, "{}", x),
+            Value::String(x) => write!(f, "\"{}\"", x),
+            Value::Integer(x) => write!(f, "{}", x),
+            Value::Float(x) => write!(f, "{}", x),
+        }
+    }
 }
 
 /// Interprets a Sail expression, returning the result
