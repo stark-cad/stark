7e572c5a48b380081113d5fab2c6030d105d84ea
Matthew Rothlisberger <mattjrothlis@gmail.com>
2024-03-03 22:26:40 -0500

    updates to latest released winit version

--- src/context.rs
+++ src/context.rs
@@ -18,11 +18,12 @@
 
 use crate::{sail, Frame};
 
-use png;
+// use png;
 use winit::{
     dpi,
-    event::{self, DeviceEvent, ElementState, Event, VirtualKeyCode, WindowEvent},
+    event::{DeviceEvent, ElementState, Event, KeyEvent, RawKeyEvent, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    keyboard::{Key, KeyCode, NamedKey, PhysicalKey},
     window::{self, WindowBuilder},
 };
 
@@ -36,13 +37,15 @@ pub fn init_context(
     width: u32,
     height: u32,
 ) -> (Frame, EventLoop<()>) {
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new().unwrap();
     let frame = WindowBuilder::new()
         .with_title(title)
         .with_window_icon(get_icon(icon_file))
         .with_inner_size(dpi::Size::Physical(dpi::PhysicalSize { width, height }))
         .build(&event_loop)
         .unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
 
     (frame, event_loop)
 }
@@ -95,161 +98,249 @@ pub fn run_loop<Ij: 'static>(
 
     let mut focus = false;
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::LoopDestroyed => {
-                joins.take().unwrap().for_each(|x| x.join().unwrap());
-            }
-
-            Event::RedrawRequested(..) => {
-                let redrw = sail::sym_init(sl_reg, sail::K_CX_REDRW.0);
-                sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), redrw);
-            }
-
-            Event::RedrawEventsCleared => {
-                // let redrw = sail::sym_init(sl_reg, sail::K_CX_REDRW.0);
-                // sail::queue::queue_tx(rndr_tx.clone(), redrw);
-            }
-
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::Exit;
-
-                    let destr = sail::sym_init(sl_reg, sail::K_CX_DESTR.0);
-
-                    sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), destr.clone());
-                    sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), destr);
+    event_loop
+        .run(move |event, elwt| {
+            match event {
+                Event::LoopExiting => {
+                    joins.take().unwrap().for_each(|x| x.join().unwrap());
                 }
-                WindowEvent::Focused(f) => {
-                    focus = f;
-                }
-                WindowEvent::Resized(dims) => {
-                    frame_dims = [dims.width, dims.height];
-                    sail::arrvec_rplc(fr_dims.clone(), &[dims.width, dims.height]);
 
-                    let resiz = sail::sym_init(sl_reg, sail::K_CX_RESIZ.0);
-                    sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), resiz);
-                }
-                WindowEvent::ScaleFactorChanged {
-                    new_inner_size: dims,
-                    ..
-                } => {
-                    frame_dims = [dims.width, dims.height];
-                    sail::arrvec_rplc(fr_dims.clone(), &[dims.width, dims.height]);
-
-                    let resiz = sail::sym_init(sl_reg, sail::K_CX_RESIZ.0);
-                    sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), resiz);
-                }
-                WindowEvent::MouseInput { state, button, .. } => {
-                    if state == ElementState::Pressed {
-                        let recrd = sail::sym_init(sl_reg, sail::K_CX_RECRD.0);
-                        sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), recrd);
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::RedrawRequested => {
+                        let redrw = sail::sym_init(sl_reg, sail::K_CX_REDRW.0);
+                        sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), redrw);
                     }
-                }
-                WindowEvent::CursorMoved {
-                    position: dpi::PhysicalPosition { x, y },
-                    ..
-                } => {
-                    sail::arrvec_rplc(
-                        cur_pos.clone(),
-                        &[
-                            (x / (frame_dims[0] / 2) as f64 - 1.0) as f32,
-                            (y / (frame_dims[1] / 2) as f64 - 1.0) as f32,
-                        ],
-                    );
+                    WindowEvent::CloseRequested => {
+                        elwt.exit();
 
-                    let moved = sail::sym_init(sl_reg, sail::K_CX_CURMV.0);
+                        let destr = sail::sym_init(sl_reg, sail::K_CX_DESTR.0);
 
-                    // TODO: intern symbols by default, copy out into
-                    // lists as necessary
+                        sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), destr.clone());
+                        sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), destr);
+                    }
+                    WindowEvent::Focused(f) => {
+                        focus = f;
+                    }
+                    WindowEvent::Resized(dims) => {
+                        frame_dims = [dims.width, dims.height];
+                        sail::arrvec_rplc(fr_dims.clone(), &[dims.width, dims.height]);
 
-                    // TODO: in context thread, maintain one copy of
-                    // each keyword symbol, that gets "dispatched"
-                    // every time there is a message; the queue system
-                    // copies symbols into the receiving environment
+                        let resiz = sail::sym_init(sl_reg, sail::K_CX_RESIZ.0);
+                        sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), resiz);
+                    }
+                    WindowEvent::MouseInput {
+                        state, button: _, ..
+                    } => {
+                        if state == ElementState::Pressed {
+                            let recrd = sail::sym_init(sl_reg, sail::K_CX_RECRD.0);
+                            sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), recrd);
+                        }
+                    }
+                    WindowEvent::CursorMoved {
+                        position: dpi::PhysicalPosition { x, y },
+                        ..
+                    } => {
+                        sail::arrvec_rplc(
+                            cur_pos.clone(),
+                            &[
+                                (x / (frame_dims[0] / 2) as f64 - 1.0) as f32,
+                                (y / (frame_dims[1] / 2) as f64 - 1.0) as f32,
+                            ],
+                        );
 
-                    sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), moved);
-                }
-                _ => {}
-            },
+                        let moved = sail::sym_init(sl_reg, sail::K_CX_CURMV.0);
 
-            Event::DeviceEvent { event, .. } => match event {
-                DeviceEvent::Key(event::KeyboardInput {
-                    scancode,
-                    state,
-                    virtual_keycode,
-                    ..
-                }) => {
-                    if focus && state == ElementState::Pressed {
-                        match virtual_keycode {
-                            Some(VirtualKeyCode::U) => {
+                        // TODO: intern symbols by default, copy out into
+                        // lists as necessary
+
+                        // TODO: in context thread, maintain one copy of
+                        // each keyword symbol, that gets "dispatched"
+                        // every time there is a message; the queue system
+                        // copies symbols into the receiving environment
+
+                        sail::queue::queue_tx(dummy_env.clone(), rndr_tx.clone(), moved);
+                    }
+                    WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key, state, ..
+                            },
+                        ..
+                    } if focus && state == ElementState::Pressed => match logical_key {
+                        Key::Character(s) => match s.bytes().nth(0).unwrap() {
+                            b'u' => {
                                 // move up
                                 let key_u = sail::sym_init(sl_reg, sail::K_CX_KEY_U.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_u);
                             }
-                            Some(VirtualKeyCode::D) => {
+                            b'd' => {
                                 // move down
                                 let key_d = sail::sym_init(sl_reg, sail::K_CX_KEY_D.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_d);
                             }
-                            Some(VirtualKeyCode::F) => {
+                            b'f' => {
                                 // move forward (right)
                                 let key_f = sail::sym_init(sl_reg, sail::K_CX_KEY_F.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_f);
                             }
-                            Some(VirtualKeyCode::B) => {
+                            b'b' => {
                                 // move backward (left)
                                 let key_b = sail::sym_init(sl_reg, sail::K_CX_KEY_B.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_b);
                             }
-                            Some(VirtualKeyCode::L) => {
+                            b'l' => {
                                 // make step longer
                                 let key_l = sail::sym_init(sl_reg, sail::K_CX_KEY_L.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_l);
                             }
-                            Some(VirtualKeyCode::S) => {
+                            b's' => {
                                 // make step shorter
                                 let key_s = sail::sym_init(sl_reg, sail::K_CX_KEY_S.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_s);
                             }
-                            Some(VirtualKeyCode::E) => {
+                            b'e' => {
                                 // escape line in progress
                                 let key_e = sail::sym_init(sl_reg, sail::K_CX_KEY_E.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_e);
                             }
-                            Some(VirtualKeyCode::K) => {
+                            b'k' => {
                                 // kill last line drawn
                                 let key_k = sail::sym_init(sl_reg, sail::K_CX_KEY_K.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_k);
                             }
-                            Some(VirtualKeyCode::M) => {
+                            b'm' => {
                                 // switch drawing mode
                                 let key_m = sail::sym_init(sl_reg, sail::K_CX_KEY_M.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), key_m);
                             }
-                            Some(VirtualKeyCode::Space) => {
+                            _ => {}
+                        },
+                        Key::Named(n) => match n {
+                            NamedKey::Space => {
                                 // enter the point
                                 let recrd = sail::sym_init(sl_reg, sail::K_CX_RECRD.0);
                                 sail::queue::queue_tx(dummy_env.clone(), main_tx.clone(), recrd);
                             }
                             _ => {}
-                        }
-                    }
-                }
+                        },
+                        _ => {}
+                    },
+                    _ => {}
+                },
+
+                // Event::DeviceEvent { event, .. } => match event {
+                //     DeviceEvent::Key(RawKeyEvent {
+                //         physical_key: PhysicalKey::Code(kc),
+                //         state,
+                //     }) => {
+                //         if focus && state == ElementState::Pressed {
+                //             match kc {
+                //                 KeyCode::KeyU => {
+                //                     // move up
+                //                     let key_u = sail::sym_init(sl_reg, sail::K_CX_KEY_U.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_u,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyD => {
+                //                     // move down
+                //                     let key_d = sail::sym_init(sl_reg, sail::K_CX_KEY_D.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_d,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyF => {
+                //                     // move forward (right)
+                //                     let key_f = sail::sym_init(sl_reg, sail::K_CX_KEY_F.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_f,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyB => {
+                //                     // move backward (left)
+                //                     let key_b = sail::sym_init(sl_reg, sail::K_CX_KEY_B.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_b,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyL => {
+                //                     // make step longer
+                //                     let key_l = sail::sym_init(sl_reg, sail::K_CX_KEY_L.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_l,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyS => {
+                //                     // make step shorter
+                //                     let key_s = sail::sym_init(sl_reg, sail::K_CX_KEY_S.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_s,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyE => {
+                //                     // escape line in progress
+                //                     let key_e = sail::sym_init(sl_reg, sail::K_CX_KEY_E.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_e,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyK => {
+                //                     // kill last line drawn
+                //                     let key_k = sail::sym_init(sl_reg, sail::K_CX_KEY_K.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_k,
+                //                     );
+                //                 }
+                //                 KeyCode::KeyM => {
+                //                     // switch drawing mode
+                //                     let key_m = sail::sym_init(sl_reg, sail::K_CX_KEY_M.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         key_m,
+                //                     );
+                //                 }
+                //                 KeyCode::Space => {
+                //                     // enter the point
+                //                     let recrd = sail::sym_init(sl_reg, sail::K_CX_RECRD.0);
+                //                     sail::queue::queue_tx(
+                //                         dummy_env.clone(),
+                //                         main_tx.clone(),
+                //                         recrd,
+                //                     );
+                //                 }
+                //                 _ => {}
+                //             }
+                //         }
+                //     }
                 // DeviceEvent::MouseWheel {
                 //     delta: event::MouseScrollDelta::LineDelta(xdel, ydel),
                 // } => {}
                 // DeviceEvent::MouseMotion {
                 //     delta: (xdel, ydel),
                 // } => {}
+                //     _ => {}
+                // },
                 _ => {}
-            },
-
-            _ => {}
-        }
-    });
+            }
+        })
+        .unwrap();
 }
 
 /// Retrieves an icon from a PNG file and outputs it in the format desired by `winit`
